d4dda8259e9b6a41b00f72bd9a342a3e52c9ab0f
Francesco Serafini <fserafini@een.com>
2022-06-11 20:14:45 +0200

    separate entrypoints

---
+++ entrypoint_success.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+# Define command as string
+cmd = 'ls -ltr'
+
+# Use shell to execute the command and store it in sp variable
+sp = subprocess.Popen(cmd,shell=True)
+
+# Store the return code in rc variable
+rc=sp.wait()
+
+# Print the content of sp variable
+print(sp)
+
+sys.exit(0)
